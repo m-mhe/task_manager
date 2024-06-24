a2c568a71c95df9c0745cb83199d84cc9bf47e68
Momin Hosan Emon <mominhasanemon@gmail.com>
2024-06-24 19:17:53 +0600

    some changes here and there

--- lib/UI/screens/authentication/sign_in_screen.dart
+++ lib/UI/screens/authentication/sign_in_screen.dart
@@ -2,8 +2,13 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager/UI/screens/authentication/email_for_resetting_password_screen.dart';
 import 'package:task_manager/UI/screens/authentication/sign_up_screen.dart';
+import 'package:task_manager/UI/utility/URLList.dart';
+import 'package:task_manager/UI/utility/validator.dart';
 import 'package:task_manager/UI/widgets/background_widget.dart';
 import 'package:task_manager/UI/widgets/bottom_navigation_bar.dart';
+import 'package:task_manager/UI/widgets/snack_bar_message.dart';
+import 'package:task_manager/data/model/api_response.dart';
+import 'package:task_manager/data/network_caller/api_call.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -24,70 +29,100 @@ class _SignInScreenState extends State<SignInScreen> {
                   left: MediaQuery.sizeOf(context).width / 10,
                   right: MediaQuery.sizeOf(context).width / 10,
                   top: MediaQuery.sizeOf(context).height / 4),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    textAlign: TextAlign.center,
-                    'Sign In',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: _tEcEmail,
-                    decoration: const InputDecoration(
-                      hintText: 'Email',
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      textAlign: TextAlign.center,
+                      'Sign In',
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: _tEcPassword,
-                    decoration: const InputDecoration(
-                      hintText: 'Password',
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  ElevatedButton(
-                      onPressed: _onTapNewTaskScreen,
-                      child: const Icon(Icons.arrow_circle_right_outlined)),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextButton(
-                    onPressed: _onTapEmailVerificationScreen,
-                    child: Text(
-                      'Forget Password?',
-                      style: Theme.of(context).textTheme.bodySmall,
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (v) {
+                        if (v!.trim().isEmpty) {
+                          return 'Kindly enter your email-address!';
+                        }
+                        if (Validator.emailValidator.hasMatch(v) == false) {
+                          return 'Kindly enter valid email-address.';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _tEcEmail,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),
                     ),
-                  ),
-                  RichText(
-                    text: TextSpan(
-                      text: "Don't have an account?",
-                      style: Theme.of(context).textTheme.titleSmall,
-                      children: [
-                        TextSpan(
-                          text: ' Sign up',
-                          style: const TextStyle(
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (v) {
+                        if (v!.trim().isEmpty) {
+                          return 'Kindly enter your pass key!';
+                        }
+                        return null;
+                      },
+                      controller: _tEcPassword,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Visibility(
+                      visible: notLoading,
+                      replacement: const SizedBox(
+                          height: 25,
+                          width: 25,
+                          child: CircularProgressIndicator(
                             color: Color(0xff21BF73),
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = _onTapSignUpScreen,
-                        ),
-                      ],
+                          )),
+                      child: ElevatedButton(
+                          onPressed: _onTapNewTaskScreen,
+                          child: const Icon(Icons.arrow_circle_right_outlined)),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                ],
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextButton(
+                      onPressed: _onTapEmailVerificationScreen,
+                      child: Text(
+                        'Forget Password?',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "Don't have an account?",
+                        style: Theme.of(context).textTheme.titleSmall,
+                        children: [
+                          TextSpan(
+                            text: ' Sign up',
+                            style: const TextStyle(
+                              color: Color(0xff21BF73),
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = _onTapSignUpScreen,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -99,8 +134,48 @@ class _SignInScreenState extends State<SignInScreen> {
   //=======================================================VARIABLES=======================================================
   final TextEditingController _tEcEmail = TextEditingController();
   final TextEditingController _tEcPassword = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool notLoading = true;
 
   //=======================================================FUNCTIONS=======================================================
+  Future<void> signInServer() async {
+    setState(() {
+      notLoading = false;
+    });
+    final Map<String, String> userSignInData = {
+      "email": _tEcEmail.text,
+      "password": _tEcPassword.text
+    };
+    ApiResponse getServerResponse =
+        await ApiCall.postResponse(URLList.logInURL, userSignInData);
+    if (getServerResponse.isSuccess == true) {
+      if (mounted) {
+        bottomPopUpMessage(
+          context,
+          'Sign In Success!',
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const BottomNavBar(),
+          ),
+        );
+      }
+      setState(() {
+        notLoading = true;
+      });
+    } else {
+      if (mounted) {
+        bottomPopUpMessage(context, 'Kindly check your email or password!',
+            showError: true);
+        await Future.delayed(const Duration(seconds: 2));
+        setState(() {
+          notLoading = true;
+        });
+      }
+    }
+  }
+
   void _onTapEmailVerificationScreen() {
     Navigator.push(
       context,
@@ -119,13 +194,10 @@ class _SignInScreenState extends State<SignInScreen> {
     );
   }
 
-  void _onTapNewTaskScreen() {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const BottomNavBar(),
-      ),
-    );
+  Future<void> _onTapNewTaskScreen() async {
+    if (_formKey.currentState!.validate()) {
+      await signInServer();
+    }
   }
 
   @override
--- lib/UI/utility/URLList.dart
+++ lib/UI/utility/URLList.dart
@@ -1,4 +1,5 @@
 class URLList{
   static const String _baseURL = 'https://task.teamrabbil.com/api/v1';
   static const String registrationURL = '$_baseURL/registration';
+  static const String logInURL = '$_baseURL/login';
 }
